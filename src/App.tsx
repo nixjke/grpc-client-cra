48a0ceaf95e1283193668a2382a77024948747cc
nixjke <aziz.10.255@gmail.com>
2023-06-18 16:58:26 +0300

    request add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import logo from './logo.svg'
 import './App.css'
+import { HeroServiceClient } from './proto/HeroServiceClientPb'
+import { useEffect } from 'react'
+import { HeroById } from './proto/hero_pb'
+
+const client = new HeroServiceClient('localhost:3000')
 
 function App() {
+  const request = new HeroById()
+  request.setId(1)
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await client.findOne(request, {})
+      console.log('@res', response)
+    })()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
